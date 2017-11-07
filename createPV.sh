cfb8a7dc7349b881110ab22b528945edb11dc7c5
Matt Pays <matt.pays@fasthosts.com>
2017-11-07 14:23:34 +0000

    shellcheck'd

--- createPV.sh
+++ createPV.sh
@@ -9,25 +9,25 @@ VOLYAML=$(mktemp /tmp/vol.XXXXXXX.yaml)
 while IFS=$',' read -r project dataname size ; do
 
 	#create dataset - create parent manually before running this script
-	zfs create $ZPOOL/${project}/${dataname}
+	zfs create $ZPOOL/"${project}"/"${dataname}"
 	
 	#dirty haxx
-	chgrp -R 0 /$ZPOOL/${project}/${dataname}
-	chmod -R go+rwx /$ZPOOL/${project}/${dataname}
+	chgrp -R 0 /$ZPOOL/"${project}"/"${dataname}"
+	chmod -R go+rwx /$ZPOOL/"${project}"/"${dataname}"
 
 	#set SELinux context
-	chcon -Rt svirt_sandbox_file_t /$ZPOOL/${project}/${dataname}
+	chcon -Rt svirt_sandbox_file_t /$ZPOOL/"${project}"/"${dataname}"
 
 	# Yawn....
 	sleep 5
 
 	#modify vol.yaml & oc create it
-	cp -Zf vol.yaml.default $VOLYAML
-	sed -i "s/VOL_NAME/${dataname}/g" $VOLYAML 
-	sed -i "s/VOL_PATH/$ZPOOL\/${project}\/${dataname}/g" $VOLYAML
-	sed -i "s/VOL_SIZE/${size}/g" $VOLYAML
-	sed -i "s/VOL_PROJECT/${project}/g" $VOLYAML
-	oc create -f $VOLYAML
+	cp -Zf vol.yaml.default "$VOLYAML"
+	sed -i "s/VOL_NAME/${dataname}/g" "$VOLYAML"
+	sed -i "s/VOL_PATH/$ZPOOL\/${project}\/${dataname}/g" "$VOLYAML"
+	sed -i "s/VOL_SIZE/${size}/g" "$VOLYAML"
+	sed -i "s/VOL_PROJECT/${project}/g" "$VOLYAML"
+	oc create -f "$VOLYAML"
 
 	#add to /etc/exports & refresh mount list
 	echo "/$ZPOOL/${project}/${dataname}	*(rw,sync,no_root_squash,no_all_squash)" >> /etc/exports
